7884bf46e63e9aa27d6de97b9aa16a5987adb567
Alok Irde <alok.irde@emc.com>
2016-09-05 15:35:22 -0700

    Updated ARC notes

--- swift-language.playground/Pages/ARC.xcplaygroundpage/Contents.swift
+++ swift-language.playground/Pages/ARC.xcplaygroundpage/Contents.swift
@@ -4,6 +4,17 @@ import Foundation
 
 var str = "ARC"
 
+/*
+ IMPORTANT NOTES
+ 
+ 1. object instances marked with unowned references are not optionals, it just means that they do not have a strong reference
+ 2. Use unowned reference when both the referencing object and the referenced object can be deallocated at the same time
+ 3. Use weak when its possible that the referenced object might get deallocated during the course of execution of the method context.
+ 4. CAPTURE LISTS are important when trying to break reference cycles in closures. pass the list of object instances along with either a weak or unowned reference that might be necessary in that closure context.
+ */
+
+
+
 //------------------------------------------------------------------------------------//
 // ARC in action
 //------------------------------------------------------------------------------------//
@@ -74,6 +85,42 @@ class Apartment {
 var tenant = Tenant(name: "Tenant1")
 var apartment = Apartment(unitName: "Apartment1", tenant: tenant)
 tenant.apartment = apartment
+
+
+//------------------------------------------------------------------------------------//
+// CLOSURES & REFERENCES
+//------------------------------------------------------------------------------------//
+
+/*
+ In this example the use of self within the asHTML closure causes the closure to hold a strong reference
+ Thus even if header instance is set to nil, that wouldnt release the reference to it, causing a memory leak
+ The way around this is to use CAPTURE LISTS
+ */
+class HTMLElement {
+    var name: String
+    var text: String?
+    
+    init(name: String, text: String?) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("De-initializing \(self.name)")
+    }
+    
+    lazy var asHTML: () -> String = { [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "</\(self.name)>"
+        }
+    }
+}
+
+var header: HTMLElement? = HTMLElement(name: "h1", text: "Ola")
+header!.asHTML()
+header = nil
 
 
 
