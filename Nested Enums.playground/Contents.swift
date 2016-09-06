8d5061c783fdd17e5054b97e4f5ee3876b82f160
Alok Irde <alok.irde@emc.com>
2016-09-05 18:34:15 -0700

    Enum Deep Dive Playground

---
+++ Nested Enums.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Enum deep dive"
+
+/*
+ Enums can be nested as shown in the example below to enable collecting relevant information together.
+ */
+
+enum Family {
+    case Lannister
+    case Stark
+    case Arryn
+    case Tully
+    case Baratheon
+    case Tyrell
+    case Martell
+    
+    enum Sigil {
+        case Lion
+        case Arrows
+        case Broadsword
+        case None
+    }
+    
+    enum Bannermen {
+        case LannisterBannermen
+        case StarkBannermen
+        case ArrynBannermen
+        case TullyBannermen
+        case None
+    }
+}
+
+struct Kingdom {
+    var family: Family
+    var sigil: Family.Sigil
+    var bannermen: Family.Bannermen
+}
+
+let lannister = Kingdom(family: .Lannister, sigil: .Lion, bannermen: .LannisterBannermen)
+
+
+
+
+
